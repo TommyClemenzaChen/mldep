d82d818b01277587bb55a0a59bc8e9efc7aa687e
Tommy Chen <tommyc8011@gmail.com>
2024-06-08 01:17:09 -0700

    changed author name

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_requirements(file_path:str) -> List[str]:
 setup(
     name = 'mlproject',
     version='0.0.1',
-    author='tommy',
+    author='Tommy Chen',
     author_email='tommyc8011@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
