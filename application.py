656648573c9b9bc09573ae0733f4af3901e56c26
Tommy Chen <tommyc8011@gmail.com>
2024-06-09 14:31:22 -0700

    adding cors

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
+from flask_cors import CORS,cross_origin
+
 
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import PredictPipeline, CustomData
@@ -10,10 +12,12 @@ app = application
 
 ## Route for home page
 @app.route('/')
+@cross_origin()
 def index(): 
     return render_template('index.html')
 
 @app.route('/predict', methods=['GET', 'POST'])
+@cross_origin()
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -24,8 +28,8 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            writing_score=float(request.form.get('writing_score')),
+            reading_score=float(request.form.get('reading_score'))
 
         )
         pred_df=data.get_data_as_data_frame()
